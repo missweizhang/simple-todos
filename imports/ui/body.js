9d1c220d9942b70ab8a1824632d9d4ffffb442fa
Wei Zhang <missweizhang@gmail.com>
2017-04-12 17:45:01 -0700

    hide completed tasks

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,23 +1,39 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+    console.log(this);
+    this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
     // tasks from database collection
     tasks() {
-        // show newest tasks at the top
+        const instance = Template.instance();
+        if (instance.state.get('hideCompleted'))
+        {
+            // if hide completed is checked, filter tasks
+            // { $ne: true } OR { $eq: false }
+            return Tasks.find({ checked: { $ne: true }}, { sort: { createdAt: -1} });
+        }
+
+        // otherwise, returan all of the tasks, show newest tasks at the top
         return Tasks.find({}, { sort: { createdAt: -1 } });
     },
-    
 /*  // tasks as static array
     tasks: [
         { text: 'This is task 1' },
         { text: 'This is task 2' },
         { text: 'This is task 3' },
-    ], */
+        ], */
+    incompleteCount() {
+        return Tasks.find({ checked: { $ne: true } }).count();
+    },
 });
 
 Template.body.events({
@@ -38,5 +54,10 @@ Template.body.events({
 
         // clear form
         target.text.value = '';
+    },
+    'click .hide-completed'(event, instance) {
+//    'change .hide-completed input'(event, instance) {
+        console.log(event.target);
+        instance.state.set('hideCompleted', event.target.checked);
     },
 });
