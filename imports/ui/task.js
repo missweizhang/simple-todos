6e6322283a16c280da57a65b29b38c79db4a5d99
Wei Zhang <missweizhang@gmail.com>
2017-04-22 16:26:18 -0700

    private tasks allow edits by owner only

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -2,6 +2,12 @@ import { Template } from 'meteor/templating';
 
 import './task.html';
 
+Template.task.helpers({
+    isOwner() {
+        return this.owner == Meteor.userId();
+    },
+});
+
 Template.task.events({
     'click .toggle-checked'() {
         // set the checked property to the opposite of its current value
@@ -10,4 +16,7 @@ Template.task.events({
     'click .delete'() {
         Meteor.call('tasks.remove', this._id);
     },
+    'click .toggle-private'() {
+        Meteor.call('tasks.setPrivate', this._id, !this.private);
+    }
 });
